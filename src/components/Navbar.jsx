7df6b2157e3723752c2184900488adf55f4290b0
areil <maldonadoaa.91@gmail.com>
2023-02-21 04:00:12 -0300

    fix img3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,27 +11,27 @@ const Navbar = () => {
         Ariel Maldonado
       </div>
       <div className={`nav_items ${isOpen && "open"}`}>
-        <a href="#home"> <i className="fa-sharp fa-solid fa-house"></i> Inicio</a>
-        <a href="#about"><i className="fa-regular fa-user"></i> Acerca de mi</a>
-        <a href="#skills"><i className="fa-solid fa-list-check"></i> Habilidades</a>
-        <a href="#projects"><i className="fa-solid fa-diagram-project"></i> Proyectos</a>
-        <a href="#contact"><i className="fa-regular fa-envelope"></i> Contacto</a>
+        <a href="#home" onClick={() => setIsOpen(!isOpen)}> <i className="fa-sharp fa-solid fa-house"></i> Inicio</a>
+        <a href="#about" onClick={() => setIsOpen(!isOpen)}><i className="fa-regular fa-user"></i> Acerca de mi</a>
+        <a href="#skills" onClick={() => setIsOpen(!isOpen)}><i className="fa-solid fa-list-check"></i> Habilidades</a>
+        <a href="#projects" onClick={() => setIsOpen(!isOpen)}><i className="fa-solid fa-diagram-project"></i> Proyectos</a>
+        <a href="#contact" onClick={() => setIsOpen(!isOpen)}><i className="fa-regular fa-envelope"></i> Contacto</a>
         <div className='nav-social'>
-          <div className='social-media'>
+          <div className='social-media' onClick={() => setIsOpen(!isOpen)}>
             <abbr title="Linkedin">
               <a href="https://www.linkedin.com/in/ariel-alejandro-maldonado-68276224b/" target={'_blank'} className='social-media-icon'>
                 <i className='bx bxl-linkedin' ></i>
               </a>
             </abbr>
           </div>
-          <div className='social-media'>
+          <div className='social-media' onClick={() => setIsOpen(!isOpen)}>
             <abbr title="Git Hub">
               <a href="https://github.com/ariel2319" target={'_blank'} className='social-media-icon'>
                 <i className='bx bxl-github' ></i>
               </a>
             </abbr>
           </div>
-          <div className='social-media'>
+          <div className='social-media' onClick={() => setIsOpen(!isOpen)}>
             <abbr title="CV">
               <a href="https://drive.google.com/file/d/1TwXGwlmLz1_jLuiG79S0GYlExSR4ESyL/view" target={'_blank'} className='social-media-icon'>
                 <i className='bx bx-id-card'></i>
